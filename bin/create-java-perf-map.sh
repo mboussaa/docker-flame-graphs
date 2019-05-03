0d7c65b279a99219513adcc0ad8c2d6a9883df49
mboussaa <mohamedboussaa3@gmail.com>
2019-05-03 10:56:19 +0200

    Beta version: integrate a camp example + tests

--- bin/create-java-perf-map.sh
+++ bin/create-java-perf-map.sh
@@ -10,12 +10,13 @@ PERF_MAP_DIR=$(dirname $(readlink -f $0))/..
 ATTACH_JAR_PATH=$PERF_MAP_DIR/out/$ATTACH_JAR
 PERF_MAP_FILE=/tmp/perf-$PID.map
 
-if [ -z "$JAVA_HOME" ]; then
-  JAVA_HOME=/usr/lib/jvm/default-java
-fi
+#if [ -z "$JAVA_HOME" ]; then
+#  JAVA_HOME=/usr/lib/jvm/default-java
+#fi
 
-[ -d "$JAVA_HOME" ] || JAVA_HOME=/etc/alternatives/java_sdk
-[ -d "$JAVA_HOME" ] || (echo "JAVA_HOME directory at '$JAVA_HOME' does not exist." && false)
+#[ -d "$JAVA_HOME" ] || JAVA_HOME=/etc/alternatives/java_sdk
+#[ -d "$JAVA_HOME" ] || (echo "JAVA_HOME directory at '$JAVA_HOME' does not exist." && false)
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 
 rm $PERF_MAP_FILE -f
 (cd $PERF_MAP_DIR/out && java -cp $ATTACH_JAR_PATH:$JAVA_HOME/lib/tools.jar net.virtualvoid.perf.AttachOnce $PID "$OPTIONS")
